20ddfe4e3debda0a81d9af65a4d37e6a8cd7301f
Afzal <kapadwalahajimohammedafzal@gmail.com>
2025-04-03 16:29:28 +0530

    fix links and variables

---
+++ 07_projects/04_GuessTheNumber/script.js
@@ -0,0 +1,9 @@
+const randomNumber = parseInt(Math.random() * 10 + 1);
+
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+const lowORHi = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
+
